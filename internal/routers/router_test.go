ac59828c0a5cf3eb6b94d96d88bfe5e0da71bdb4
Asimro-B <asimrobitaye23@gmmail.com>
2026-07-03 10:56:26 +0300

    small test for testing tenat enpoints added

---
+++ internal/routers/router_test.go
@@ -0,0 +1,17 @@
+package route
+
+import (
+	handler "restaurant-platform/internal/handlers"
+	"testing"
+
+	"github.com/gin-gonic/gin"
+)
+
+func TestRegisterRoutes(t *testing.T) {
+	gin.SetMode(gin.TestMode)
+
+	r := gin.New()
+	api := r.Group("/api/v1")
+
+	RegisterRoutes(api, handler.NewWebHandler(nil))
+}
