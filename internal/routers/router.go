43150ea406315ce084bf9fffd7fe9a53e7de15bf
Asimro-B <asimrobitaye23@gmmail.com>
2026-07-03 10:56:45 +0300

    tenant enpoints route registered

--- internal/routers/router.go
+++ internal/routers/router.go
@@ -8,6 +8,12 @@ import (
 
 func RegisterRoutes(r *gin.RouterGroup, h *handler.WebHandler) {
 	r.POST("/tenant", h.CreateTenant)
+	r.GET("/tenant/slug/:slug", h.GetTenantBySlug)
+	r.GET("/tenants", h.ListTenants)
+	r.GET("/tenants/:tenantID", h.GetTenantByID)
+	r.PUT("/tenants/:tenantID", h.UpdateTenant)
+	r.DELETE("/tenants/:tenantID", h.DeleteTenant)
+	r.PATCH("/tenants/:tenantID/restore", h.RestoreTenant)
 	r.GET("/tenants/:tenantID/user/get-by-email", h.GetUserByEmail)
 	r.POST("/tenants/:tenatID/user", h.CreateUser)
 }
