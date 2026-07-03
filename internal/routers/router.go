ccae3b9dac5097be38c7e3c2982b8502033bff61
Asimro-B <asimrobitaye23@gmmail.com>
2026-07-03 16:01:04 +0300

    route updated to require token

--- internal/routers/router.go
+++ internal/routers/router.go
@@ -2,18 +2,34 @@ package route
 
 import (
 	handler "restaurant-platform/internal/handlers"
+	"restaurant-platform/internal/middleware"
 
 	"github.com/gin-gonic/gin"
 )
 
 func RegisterRoutes(r *gin.RouterGroup, h *handler.WebHandler) {
+	// Public no token needed
+	r.POST("/auth/login", h.HandleLogin)
 	r.POST("/tenant", h.CreateTenant)
-	r.GET("/tenant/slug/:slug", h.GetTenantBySlug)
-	r.GET("/tenants", h.ListTenants)
-	r.GET("/tenants/:tenantID", h.GetTenantByID)
-	r.PUT("/tenants/:tenantID", h.UpdateTenant)
-	r.DELETE("/tenants/:tenantID", h.DeleteTenant)
-	r.PATCH("/tenants/:tenantID/restore", h.RestoreTenant)
-	r.GET("/tenants/:tenantID/user/get-by-email", h.GetUserByEmail)
-	r.POST("/tenants/:tenatID/user", h.CreateUser)
+
+	// protected token required
+	protected := r.Group("/")
+	protected.Use(middleware.RequireAuth())
+	{
+		// Tenant management
+		protected.GET("/tenant/slug/:slug", h.GetTenantBySlug)
+		protected.GET("/tenants", h.ListTenants)
+		protected.GET("/tenants/:tenantID", h.GetTenantByID)
+		protected.PUT("/tenants/:tenantID", h.UpdateTenant)
+		protected.DELETE("/tenants/:tenantID", h.DeleteTenant)
+		protected.PATCH("/tenants/:tenantID/restore", h.RestoreTenant)
+
+		// User management
+		protected.GET("/tenants/:tenantID/users", h.ListUsers)
+		protected.GET("/tenants/:tenantID/users/:userID", h.GetUserByID)
+		protected.PUT("/tenants/:tenantID/users/:userID", h.UpdateUser)
+		protected.DELETE("/tenants/:tenantID/users/:userID", h.DeleteUser)
+		protected.GET("/tenants/:tenantID/user/get-by-email", h.GetUserByEmail)
+		protected.POST("/tenants/:tenantID/user", h.CreateUser)
+	}
 }
